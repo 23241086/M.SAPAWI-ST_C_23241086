f02c00eaca7d35bf9229fdcae60c2f523a17f2e8
23241086 <147676409+23241086@users.noreply.github.com>
2024-07-04 13:53:40 +0800

    Create Meet6.py

---
+++ Meet6.py
@@ -0,0 +1,38 @@
+# perbandingan
+# lebih dari >
+# kurang dari <
+# lebih dari sama dengan >=
+# kurang dari sama dengan <=
+# sama dengan ==
+# tidak sama dengan !=
+# is
+# is not
+
+
+a = 3
+b = 6
+
+hasil = a > b
+print (a,'>',b,'=', hasil)
+
+hasil = a < b
+print (a,'<',b,'=' ,hasil)
+hasil = a >= b
+print (a,'>=',b,'=' ,hasil)
+
+# lebih dari sama dengan >=
+hasil = a <= 4
+print (a,'<=',4,'=' , hasil)
+
+# is
+hasil = a is b
+print (a,'is',b,'=' , hasil)
+
+# is not
+hasil = a is not 4
+print (a,'is not',4,'=' , hasil)
+
+# is not
+hasil = a is not b
+print (a, 'is not',4,'=' , hasil
+)
